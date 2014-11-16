a1d3c487bba1f6a8352f782c1cdfbc12e7db362a
Jiangshui Yu <yujiangshui@gmail.com>
2014-11-16 11:22:34 +0800

    init with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			compress: {
+				'build/simple-share.min.js': ['lib/simple-share.js']
+			}
+		},
+		jshint: {
+			files: ['lib/*.js'],
+		},
+		watch: {
+			scripts: {
+				files: ['lib/*.js'],
+				tasks: ['jshint', 'uglify:compress']
+			}
+		}
+
+	});
+
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
+
+};
